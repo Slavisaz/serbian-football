77584c759d93c592ed2f744cd3050ce3b9522c8e
Slavisaz <slavisa@rogers.com>
2026-03-23 14:30:14 -0400

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("newsContent").innerText = "News loaded (placeholder)";
-document.getElementById("standingsContent").innerText = "Standings loaded (placeholder)";
-document.getElementById("videosContent").innerText = "Videos loaded (placeholder)";
