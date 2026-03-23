cc9b99d25cfa845dbb4283a9fc6457bb73927927
Slavisaz <slavisa@rogers.com>
2026-03-23 14:10:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById("newsContent").innerText = "News loaded (placeholder)";
+document.getElementById("standingsContent").innerText = "Standings loaded (placeholder)";
+document.getElementById("videosContent").innerText = "Videos loaded (placeholder)";
