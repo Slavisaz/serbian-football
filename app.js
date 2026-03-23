4ead5a6985d8627a5fbd874e0f35370cb790f638
Slavisaz <slavisa@rogers.com>
2026-03-23 16:44:57 -0400

    Refactor API calls and initialize loading functions

--- app.js
+++ app.js
@@ -2,68 +2,77 @@ const API = "https://wild-shape-c68f.slavisa-cf8.workers.dev";
 
 // NEWS
 async function loadNews() {
-  try {
-    const res = await fetch(API + "/news");
-    const data = await res.json();
+  const res = await fetch(`${API}/news`);
+  const data = await res.json();
 
-    const container = document.querySelector(".news-grid");
-    container.innerHTML = "";
+  const big = document.querySelector(".big");
+  const grid = document.querySelector(".grid");
 
-    data.slice(0,4).forEach(item => {
-      container.innerHTML += `
-        <div class="card">
-          <h4>${item.title}</h4>
-          <p>${item.source}</p>
-        </div>
-      `;
-    });
-  } catch(e) {
-    console.error("News error", e);
+  if (data.length > 0) {
+    big.innerHTML = `
+      <h2>${data[0].title}</h2>
+      <p>${data[0].source}</p>
+    `;
   }
+
+  grid.innerHTML = "";
+
+  data.slice(1).forEach(item => {
+    grid.innerHTML += `
+      <div class="card">
+        <h3>${item.title}</h3>
+        <p>${item.source}</p>
+      </div>
+    `;
+  });
 }
 
 // STANDINGS
 async function loadStandings() {
-  try {
-    const res = await fetch(API + "/standings");
-    const data = await res.json();
+  const res = await fetch(`${API}/standings`);
+  const data = await res.json();
 
-    const table = document.querySelector("table");
-    table.innerHTML = `
-      <tr><th>#</th><th>Club</th><th>Pts</th></tr>
+  const table = document.querySelector("table");
+
+  table.innerHTML = `
+    <tr><th>#</th><th>Club</th><th>Pts</th></tr>
+  `;
+
+  data.forEach((team, i) => {
+    table.innerHTML += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${team.team}</td>
+        <td>${team.points}</td>
+      </tr>
     `;
-
-    data.forEach((t,i) => {
-      table.innerHTML += `
-        <tr>
-          <td>${i+1}</td>
-          <td>${t.team}</td>
-          <td>${t.points}</td>
-        </tr>
-      `;
-    });
-  } catch(e) {
-    console.error("Standings error", e);
-  }
+  });
 }
 
 // VIDEOS
 async function loadVideos() {
-  try {
-    const res = await fetch(API + "/videos");
-    const data = await res.json();
+  const res = await fetch(`${API}/videos`);
+  const data = await res.json();
 
-    const container = document.querySelector(".video");
-    container.innerHTML = `
-      <iframe width="100%" height="200"
+  const video = document.querySelector(".video");
+
+  video.innerHTML = `
+    <h2>Video</h2>
+    <iframe
+      width="100%"
+      height="300"
       src="https://www.youtube.com/embed/${data[0].id}"
-      frameborder="0" allowfullscreen></iframe>
-    `;
-  } catch(e) {
-    console.error("Videos error", e);
-  }
+      frameborder="0"
+      allowfullscreen>
+    </iframe>
+  `;
 }
 
-loadNews();
-loadStandings();
-loadVideos();
+// INIT
+async function init() {
+  await loadNews();
+  await loadStandings();
+  await loadVideos();
+}
+
+init();
